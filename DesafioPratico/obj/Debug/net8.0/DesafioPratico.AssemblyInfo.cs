557b015bf0e8a670f6712c5b39901cc4cf72102b
evandrorl <amagicadocascalho@gmail.com>
2024-04-13 22:59:47 -0300

    teste

--- DesafioPratico/obj/Debug/net8.0/DesafioPratico.AssemblyInfo.cs
+++ DesafioPratico/obj/Debug/net8.0/DesafioPratico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesafioPratico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57fcc4541f2d29992f0f803794d9555bfe2af572")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesafioPratico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesafioPratico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
